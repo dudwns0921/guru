30cb2bb25607622f9ec0ac56d83f945be5276bd2
dudwns0921 <dudwns0921@gmail.com>
2025-07-25 20:40:49 +0900

    chore: 필요없는 backend end point 제거

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { HttpModule } from '@nestjs/axios'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
@@ -28,7 +26,5 @@ import { CourseModule } from './course/course.module'
     }),
     CourseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
