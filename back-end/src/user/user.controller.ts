fa83d18dad1c89f31087f74b78cdff3a562aca42
dudwns0921 <dudwns0921@gmail.com>
2025-08-01 00:21:59 +0900

    chore: user 도메인 필요없는 api 제거

--- back-end/src/user/user.controller.ts
+++ back-end/src/user/user.controller.ts
@@ -1,26 +1,10 @@
-import { Controller, Get, Param, Delete, UseGuards, Req } from '@nestjs/common'
+import { Controller, Delete, UseGuards, Req } from '@nestjs/common'
 import { UserService } from './user.service'
-import { User } from './user.entity'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get()
-  async findAll(): Promise<User[]> {
-    return await this.userService.findAll()
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<User | null> {
-    return await this.userService.findOne(+id)
-  }
-
-  @Get('email/:email')
-  async findByEmail(@Param('email') email: string): Promise<User | null> {
-    return await this.userService.findByEmail(email)
-  }
 
   @UseGuards(JwtAuthGuard)
   @Delete('me')
--- back-end/src/user/user.service.ts
+++ back-end/src/user/user.service.ts
@@ -10,25 +10,6 @@ export class UserService {
     private userRepository: Repository<User>,
   ) {}
 
-  async findAll(): Promise<User[]> {
-    return await this.userRepository.find({
-      where: { isActive: true },
-    })
-  }
-
-  async findOne(id: number): Promise<User | null> {
-    return await this.userRepository.findOne({
-      where: { id, isActive: true },
-      relations: ['enrollments', 'reviews', 'courseViews', 'searchHistory'],
-    })
-  }
-
-  async findByEmail(email: string): Promise<User | null> {
-    return await this.userRepository.findOne({
-      where: { email, isActive: true },
-    })
-  }
-
   async removeMe(id: number) {
     // User와 관련된 모든 데이터 삭제
     const user = await this.userRepository.findOne({
